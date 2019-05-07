751ade6402eb66d6e2a74b119f204502419f4484
jstothard <jake.stothard@arup.com>
2019-05-07 16:56:05 +0100

    create static header

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { ThemeProvider } from 'react-native-elements';
+import TitleBar from './components/TitleBar';
 
 export default class App extends React.Component {
   state = {};
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ThemeProvider>
+        <TitleBar />
+      </ThemeProvider>
     );
   }
 }
