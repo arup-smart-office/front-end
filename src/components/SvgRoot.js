d810d3c4d7f9b2a6a5027f66bbdaa1a774f7bc36
jstothard <jake.stothard@arup.com>
2019-05-13 11:29:36 +0100

    refactor prop location for svg

--- src/components/SvgRoot.js
+++ src/components/SvgRoot.js
@@ -50,88 +50,40 @@ export default class SvgRoot extends Component {
                 <Rect width="140" height="230" x="-2" fill="#FFF" stroke="#999" strokeWidth="1" />
                 <Rect width="140" height="230" x="-2" fill="rgba(40, 170, 225, 0.2)" stroke="#999" strokeWidth="1" />
                 <G id="largeDeskOne" y="50">
-                  <G key="desk0" origin="10, 17.5" rotation="180">
-                    <Desk id="0" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk1" origin="10, 17.5" rotation="180" x="20">
-                    <Desk id="1" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk2" origin="10, 17.5" rotation="180" x="40">
-                    <Desk id="2" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk3" origin="10, 17.5" rotation="180" x="60">
-                    <Desk id="3" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk4" origin="10, 17.5" y="35">
-                    <Desk id="4" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk5" origin="10, 17.5" y="35" x="20">
-                    <Desk id="5" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk6" origin="10, 17.5" y="35" x="40">
-                    <Desk id="6" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk7" origin="10, 17.5" y="35" x="60">
-                    <Desk id="7" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
+                  <Desk id="0" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="180" />
+                  <Desk id="1" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="180" x="20" />
+                  <Desk id="2" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="180" x="40" />
+                  <Desk id="3" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="180" x="60" />
+                  <Desk id="4" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" y="35" />
+                  <Desk id="5" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" y="35" x="20" />
+                  <Desk id="6" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" y="35" x="40" />
+                  <Desk id="7" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" y="35" x="60" />
                 </G>
                 <G id="largeDeskTwo" y="140">
-                  <G id="desk8" origin="10, 17.5" rotation="180">
-                    <Desk id="8" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk9" origin="10, 17.5" rotation="180" x="20">
-                    <Desk id="9" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk10" origin="10, 17.5" rotation="180" x="40">
-                    <Desk id="10" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk11" origin="10, 17.5" rotation="180" x="60">
-                    <Desk id="11" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk12" origin="10, 17.5" y="35">
-                    <Desk id="12" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk13" origin="10, 17.5" y="35" x="20">
-                    <Desk id="13" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk14" origin="10, 17.5" y="35" x="40">
-                    <Desk id="14" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk15" origin="10, 17.5" y="35" x="60">
-                    <Desk id="15" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
+                  <Desk id="8" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="180" />
+                  <Desk id="9" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="180" x="20" />
+                  <Desk id="10" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="180" x="40" />
+                  <Desk id="11" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="180" x="60" />
+                  <Desk id="12" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" y="35" />
+                  <Desk id="13" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" y="35" x="20" />
+                  <Desk id="14" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" y="35" x="40" />
+                  <Desk id="15" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" y="35" x="60" />
                 </G>
                 <G id="smallDeskOne" x="100">
-                  <G id="desk16" origin="10, 17.5" rotation="90" x="7.5">
-                    <Desk id="16" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk17" origin="10, 17.5" rotation="90" y="20" x="7.5">
-                    <Desk id="17" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
+                  <Desk id="16" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="90" x="7.5" />
+                  <Desk id="17" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="90" y="20" x="7.5" />
                 </G>
                 <G id="smallDeskTwo" x="100" y="60">
-                  <G id="desk18" origin="10, 17.5" rotation="90" x="7.5">
-                    <Desk id="18" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk19" origin="10, 17.5" rotation="90" y="20" x="7.5">
-                    <Desk id="19" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
+                  <Desk id="18" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="90" x="7.5" />
+                  <Desk id="19" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="90" y="20" x="7.5" />
                 </G>
                 <G id="smallDeskThree" x="100" y="120">
-                  <G id="desk20" origin="10, 17.5" rotation="90" x="7.5">
-                    <Desk id="20" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk21" origin="10, 17.5" rotation="90" y="20" x="7.5">
-                    <Desk id="21" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
+                  <Desk id="20" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="90" x="7.5" />
+                  <Desk id="21" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="90" y="20" x="7.5" />
                 </G>
                 <G id="smallDeskThree" x="100" y="180">
-                  <G id="desk22" origin="10, 17.5" rotation="90" x="7.5">
-                    <Desk id="22" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
-                  <G id="desk23" origin="10, 17.5" rotation="90" y="20" x="7.5">
-                    <Desk id="23" desks={desks} onPress={this.showDeskInfo} />
-                  </G>
+                  <Desk id="22" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="90" x="7.5" />
+                  <Desk id="23" desks={desks} onPress={this.showDeskInfo} origin="10, 17.5" rotation="90" y="20" x="7.5" />
                 </G>
               </G>
             </G>
