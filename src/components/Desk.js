7d9716911fdb6b19fbb8940f067633b6ca9908c7
jstothard <jake.stothard@arup.com>
2019-05-14 14:27:21 +0100

    refactor to readable code

--- src/components/Desk.js
+++ src/components/Desk.js
@@ -33,7 +33,11 @@ export default class Desk extends Component {
       currentId = String(Number(currentDesk.id) - 1);
     }
     return (
-      <G onPressIn={this.deskPressed} {... this.props} opacity={currentId !== null && currentId !== id ? 0.4 : 1}>
+      <G
+        {... this.props}
+        onPressIn={this.deskPressed}
+        opacity={currentId !== null && currentId !== id ? 0.4 : 1}
+      >
         <G id="chair" scale="0.07" x="2.5" y="21" fill={desks[id].isOccupied ? styles.occupied : styles.vacant}>
           <Path d="M161.332 22.666H54.664C48.777 22.666 44 27.447 44 33.334v64c0 17.645 14.355 32 32 32h64c17.641 0 32-14.355 32-32v-64c0-5.887-4.781-10.668-10.668-10.668z" stroke={styles.chairStroke} strokeWidth="5" />
           <Path d="M182.666 1.666c-17.641 0-32 14.355-32 32v64c0 5.887-4.777 10.668-10.664 10.668h-64c-5.891 0-10.668-4.781-10.668-10.668v-64c0-17.645-14.355-32-32-32s-32 14.355-32 32v85.332c0 29.398 23.938 53.336 53.332 53.336h106.668c29.398 0 53.332-23.937 53.332-53.336V33.666c0-17.645-14.355-32-32-32zm0 0" stroke={styles.chairStroke} strokeWidth="5" />
