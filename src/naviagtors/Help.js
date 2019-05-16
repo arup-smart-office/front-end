07f7040bf468c2ff6522ecb78d58edfffafb9ad5
jstothard <jake.stothard@arup.com>
2019-05-16 16:07:50 +0100

    change button bar

--- src/naviagtors/Help.js
+++ src/naviagtors/Help.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {
-  Alert,
   Text,
   View,
   StyleSheet,
@@ -18,37 +17,41 @@ import Contact from '../components/Contact';
 import Terms from '../components/Terms';
 import Copyright from '../components/Copyright';
 import Credits from '../components/Credits';
+import HelpNav from '../components/HelpNav';
 
-const helpStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    margin: 10,
-  },
-  buttonContainer: {
-    flex: 0.3,
-    height: 550,
-    backgroundColor: 'powderblue',
-  },
-  textContainer: {
-    marginLeft: 5,
-    flex: 0.7,
-    height: 550,
-    backgroundColor: 'grey',
-  },
-  button: {
-    alignItems: 'center',
-    paddingTop: 30,
-    paddingBottom: 30,
-  },
-  heading: {
-    alignItems: 'flex-start',
-    fontWeight: 'bold',
-    fontSize: 20,
-    margin: 5,
-    marginLeft: 10,
-  },
-});
+// const helpStyles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'row',
+//     margin: 10,
+//   },
+//   buttonContainer: {
+//     flex: 0.3,
+//     backgroundColor: 'powderblue',
+//     borderColor: 'black',
+//     borderWidth: 1,
+//     borderRadius: 5,
+//   },
+//   textContainer: {
+//     marginLeft: 5,
+//     flex: 0.7,
+//     height: '100%',
+//   },
+//   button: {
+//     alignItems: 'center',
+//     paddingTop: 30,
+//     paddingBottom: 30,
+//     borderColor: 'black',
+//     borderWidth: 1,
+//   },
+//   heading: {
+//     alignItems: 'flex-start',
+//     fontWeight: 'bold',
+//     fontSize: 20,
+//     margin: 5,
+//     marginLeft: 10,
+//   },
+// });
 
 class Help extends React.Component {
 
@@ -61,9 +64,15 @@ class Help extends React.Component {
 
   state = {
     HelpText: About,
+    Tab: 'About',
   };
 
-  handleButtonPress = (page) => {
+  handleBottomBarClick = (newTab) => {
+    this.setState({ Tab: newTab });
+  };
+
+  handleButtonPress = ({ key: page }) => {
+    this.handleBottomBarClick(page);
     if (page === 'About') {
       this.setState({ HelpText: About });
     }
@@ -88,91 +97,18 @@ class Help extends React.Component {
   }
 
   render() {
-    const { HelpText } = this.state;
-    const { navigation } = this.props;
+    const { HelpText, Tab } = this.state;
 
     return (
       <View style={styles.container}>
         <TitleBar />
-        <Text
-          style={helpStyles.heading}
-          onPress={() => {
-            navigation.navigate('Home');
-          }}
-        >
-          Help & Support
-        </Text>
-        <View style={helpStyles.container}>
-          <View style={helpStyles.buttonContainer}>
-            <TouchableOpacity
-              style={helpStyles.button}
-              onPress={event => this.handleButtonPress('About')}
-            >
-              <Text>
-                About
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={helpStyles.button}
-              onPress={event => this.handleButtonPress('User Guide')}
-            >
-              <Text>
-                User
-                Guide
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={helpStyles.button}
-              onPress={event => this.handleButtonPress('FAQs')}
-            >
-              <Text>
-                FAQs
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={helpStyles.button}
-              onPress={event => this.handleButtonPress('Contact')}
-            >
-              <Text>
-                Contact
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={helpStyles.button}
-              onPress={event => this.handleButtonPress('Terms')}
-            >
-              <Text>
-                Terms
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={helpStyles.button}
-              onPress={event => this.handleButtonPress('Copyright')}
-            >
-              <Text>
-                Copyright
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={helpStyles.button}
-              onPress={event => this.handleButtonPress('Credits')}
-            >
-              <Text>
-                Credits
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View style={helpStyles.textContainer}>
-            <HelpText />
-          </View>
+        <View>
+          <HelpText />
         </View>
+        <HelpNav activeTab={Tab} onClick={this.handleButtonPress} />
       </View>
     );
   }
 }
-
-Help.propTypes = {
-  navigation: PT.shape({ navigate: PT.func.isRequired }).isRequired,
-};
 
 export default Help;
